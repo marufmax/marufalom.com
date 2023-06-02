153dce0fd7e35cb402658898e1418eaec5b49bfd
Maruf Alom <desk.maruf@gmail.com>
2023-06-03 04:49:05 +0600

    added fevicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import LogRocket from '@/components/Logrocket'
 import ProgressBar from '@/components/ProgressBar'
 import { ScrollObserver } from '@/components/ScrollObserver'
 import siteMetadata from '@/data/siteMetadata'
-import { Analytics } from '@vercel/analytics/react'
 import { AnimatePresence } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
@@ -23,7 +22,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <LogRocket />
           <ProgressBar />
           <Component {...pageProps} />
-          <Analytics />
         </ScrollObserver>
       </AnimatePresence>
     </ThemeProvider>
