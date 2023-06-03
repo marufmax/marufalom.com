50717d30963133c9e896b2b935ee3cbdae9b13ce
Maruf Alom <desk.maruf@gmail.com>
2023-06-04 03:00:49 +0600

    removed netlify

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://cdn.logrocket.io https://cdn.lr-ingest.io https://cdn.lr-in.com https://cdn.lr-in-prod.com https://*.cloudfront.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://cdn.logrocket.io https://cdn.lr-ingest.io https://cdn.lr-in.com https://cdn.lr-in-prod.com;
   child-src 'self' blob:; 
   worker-src 'self' blob:; 
   style-src 'self' 'unsafe-inline' https://*.cloudfront.net;
   img-src * blob: data:;
   media-src 'none';
-  connect-src * https://*.logrocket.io https://*.lr-ingest.io https://*.logrocket.com https://*.lr-in.com https://*.lr-in-prod.com https://*.cloudfront.net;
+  connect-src * https://*.logrocket.io https://*.lr-ingest.io https://*.logrocket.com https://*.lr-in.com https://*.lr-in-prod.com https://vitals.vercel-insights.com;
   font-src 'self';
   frame-src giscus.app
 `
