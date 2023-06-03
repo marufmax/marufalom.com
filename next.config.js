e6a2e5dec5b1bc124c126b3fea2e2a6a12e26435
Maruf Alom <desk.maruf@gmail.com>
2023-06-03 13:00:11 +0600

    added cloundfront css

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://cdn.logrocket.io https://cdn.lr-ingest.io https://cdn.lr-in.com https://cdn.lr-in-prod.com https://*.cloudfront.net *.cloudfront.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://cdn.logrocket.io https://cdn.lr-ingest.io https://cdn.lr-in.com https://cdn.lr-in-prod.com https://*.cloudfront.net;
   child-src 'self' blob:; 
   worker-src 'self' blob:; 
-  style-src 'self' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' https://*.cloudfront.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src * https://*.logrocket.io https://*.lr-ingest.io https://*.logrocket.com https://*.lr-in.com https://*.lr-in-prod.com https://*.cloudfront.net;
