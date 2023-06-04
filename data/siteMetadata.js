18bf6c11f2a7d616853cf8084429f52f75b5f28e
Maruf Alom <desk.maruf@gmail.com>
2023-06-05 04:19:44 +0600

    wrong image

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/marufmax/marufalom.com',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.webp',
-  socialBanner: '/static/images/opengraph.webp',
+  socialBanner: '/static/images/opengraph.png',
   email: 'hi@marufalom.com',
   github: 'https://github.com/marufmax',
   twitter: 'https://twitter.com/nothingsecurity',
